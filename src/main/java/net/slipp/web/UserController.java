ddb4a0690efc531c764d8b6431a325f1de1061f9
Jiyong Kim <rlawldyd577@daum.net>
2018-07-23 16:13:00 +0900

    complete step 3-3, 3-4

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -4,25 +4,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("User : " + user.toString());
 		userRepository.save(user);
-		return "redirect:/user/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findById(id).get());
+		return "/user/updateForm";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "/user/login";
+	}
+	
+	@GetMapping("/login_failed")
+	public String login_failed() {
+		return "/user/login_failed";
+	}
+	
+	@GetMapping("/profile")
+	public String profile() {
+		return "/user/profile";
+	}
+	
+	
+	
+	
 }
