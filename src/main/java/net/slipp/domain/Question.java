6a20e49ae4e5938837aeb9bc99f9e9c7e6a0773e
Jiyong Kim <rlawldyd577@daum.net>
2018-08-01 16:57:58 +0900

    complete 6-5
    도메인 클래스에 대한 중복 제거 및 리팩토링

--- src/main/java/net/slipp/domain/Question.java
+++ src/main/java/net/slipp/domain/Question.java
@@ -1,12 +1,8 @@
 package net.slipp.domain;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
@@ -16,12 +12,7 @@ import javax.persistence.OrderBy;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-public class Question {
-	@Id
-	@GeneratedValue
-	@JsonProperty
-	private Long id;
-	
+public class Question extends AbstractEntity{
 	@ManyToOne
 	@JoinColumn(name = "fk_question_writer")
 	@JsonProperty
@@ -37,8 +28,6 @@ public class Question {
 	@JsonProperty
 	private Integer countOfAnswer = 0;
 
-	private LocalDateTime createDate;
-	
 	@OneToMany(mappedBy = "question")
 	@OrderBy("id DESC")
 	private List<Answer> answers;
@@ -50,15 +39,8 @@ public class Question {
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
-		this.createDate = LocalDateTime.now();
 	}
 	
-	public String getFormattedCreateDate() {
-		if (createDate == null) {
-			return "";
-		}
-		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
-	}
 
 	public void update(String title, String contents) {
 		this.title = title;
--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -2,40 +2,31 @@ package net.slipp.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-public class User {
-	@Id
-	@GeneratedValue
-	@JsonProperty
-	private Long id;
-	
-	@Column(nullable=false, unique=true, length=20)
+public class User extends AbstractEntity {
+	@Column(nullable = false, unique = true, length = 20)
 	@JsonProperty
 	private String userId;
-	
+
 	@JsonProperty
 	private String name;
-	
+
 	@JsonIgnore
 	private String password;
-	
+
 	@JsonProperty
 	private String email;
-	
-	
 
 	public boolean matchId(Long newId) {
 		if (newId == null) {
 			return false;
 		}
-		
-		return newId.equals(id);
+
+		return newId.equals(getId());
 	}
 
 	public String getUserId() {
@@ -45,7 +36,6 @@ public class User {
 	public String getName() {
 		return name;
 	}
-
 
 	public String getEmail() {
 		return email;
@@ -62,22 +52,17 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public boolean matchPassword(String newPassword) {
 		if (newPassword == null) {
 			return false;
 		}
-		
+
 		return newPassword.equals(password);
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
-	}	
-
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", name=" + name + ", password=" + password + ", email=" + email + "]";
 	}
 
 	public void update(User newUser) {
@@ -87,30 +72,7 @@ public class User {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public String toString() {
+		return "User [" + super.toString() + ", name=" + name + ", password=" + password + ", email=" + email + "]";
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
-
 }
